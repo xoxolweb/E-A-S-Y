71a6c502784a12707ea79e1bb848dc4de6ec5a9a
xoxolweb <xoxol.web@gmail.com>
2016-10-20 15:03:12 +0300

    slider

--- public/js/main.js
+++ public/js/main.js
@@ -4,3 +4,9 @@
 $('.choice_bl>div h5').on('click',function(){
   $(this).parent().toggleClass('onchoice')
 })
+//slider
+$('.wraper_slider').slick({
+  infinite: true,
+  autoplay: true,
+  speed: 500
+});
